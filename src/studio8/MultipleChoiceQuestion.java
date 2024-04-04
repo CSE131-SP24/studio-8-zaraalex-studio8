d200673b8a2972d88d0771be09ad9b786c7ac5e7
alexjaeger <alexjaeger@localhost>
2024-04-04 11:18:44 -0500

    hi

--- src/studio8/MultipleChoiceQuestion.java
+++ src/studio8/MultipleChoiceQuestion.java
@@ -2,8 +2,15 @@ package studio8;
 
 public class MultipleChoiceQuestion extends Question {
 	
+	private String[] choices;
+	
 	public MultipleChoiceQuestion(String prompt, String answer, int points, String[] choices) {
 		//FIXME
+		
+	super(prompt, answer, points);
+	
+	this.choices = choices;
+	
 		
 		//Call the super class constructor, then create and set
 		//instance variables for any values that aren't handled
@@ -12,6 +19,15 @@ public class MultipleChoiceQuestion extends Question {
 	
 	public void displayPrompt() {
 		//FIXME
+		
+		System.out.println(getPrompt() + "(" + getPoints() + " points)");
+		for (int i = 0; i<choices.length; i++)
+		{
+			System.out.println(i+1 + ". " + choices[i] ) ;
+		}
+
+				
+		
 		
 		/*
 		 * Use the method from the base class to display the prompt,
@@ -30,9 +46,10 @@ public class MultipleChoiceQuestion extends Question {
 		String[] choices = {"seven", "nine", "eight", "six"};
 		Question multipleChoice = new MultipleChoiceQuestion("What studio is this?", "3", 1, choices);
 		multipleChoice.displayPrompt();
+		/*
 		System.out.println(multipleChoice.checkAnswer("hi"));//wrong
 		System.out.println(multipleChoice.checkAnswer("1"));//wrong
-		System.out.println(multipleChoice.checkAnswer("3"));//right
+		System.out.println(multipleChoice.checkAnswer("3"));//right*/
 	}
 
 }
--- src/studio8/Question.java
+++ src/studio8/Question.java
@@ -31,6 +31,12 @@ public class Question {
 		return this.answer;
 	}
 	
+	public String getPrompt()
+	{
+		return this.prompt;
+		}
+	
+	
 	public static void main(String[] args) {
 		Question q = new Question("What number studio is this?", "8", 2);
 		q.displayPrompt();
